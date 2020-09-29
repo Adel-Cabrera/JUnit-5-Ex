7f05f43fa7f2419b61f5489dac1c66c51d714227
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-09-29 18:31:43 -0300

    asserted test

--- junit-5-basics/src/test/java/com/darkonnen/test1/MathUtilsTest.java
+++ junit-5-basics/src/test/java/com/darkonnen/test1/MathUtilsTest.java
@@ -1,12 +1,20 @@
 package com.darkonnen.test1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
+	
 
 	@Test
 	void test() {
-		System.out.println("this test ran");
+		
+		MathUtils mathUtils = new MathUtils();
+		int expected = 2;
+		int actual = mathUtils.add(1, 1);
+		assertEquals(expected, actual);
+		
 	}
 
 }
