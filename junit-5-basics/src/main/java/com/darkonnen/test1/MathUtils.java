7116f56dd4e433d0d41cf0d8d07c1abe2d175200
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-09-29 17:42:43 -0300

    implementing JUnit and runing tests in tests cases

---
+++ junit-5-basics/src/main/java/com/darkonnen/test1/MathUtils.java
@@ -0,0 +1,9 @@
+package com.darkonnen.test1;
+
+public class MathUtils {
+	
+	public int add(int a, int b) {
+		return a + b;		
+	}
+
+}
---
+++ junit-5-basics/src/test/java/com/darkonnen/test1/MathUtilsTest.java
@@ -0,0 +1,12 @@
+package com.darkonnen.test1;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		System.out.println("this test ran");
+	}
+
+}
