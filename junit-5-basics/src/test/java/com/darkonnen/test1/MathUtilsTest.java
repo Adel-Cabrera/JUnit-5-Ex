78eb20c4c272e07f9a00db8cc5d41bf1b50e4a75
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-09-29 21:56:16 -0300

    second test

--- junit-5-basics/src/test/java/com/darkonnen/test1/MathUtilsTest.java
+++ junit-5-basics/src/test/java/com/darkonnen/test1/MathUtilsTest.java
@@ -8,12 +8,21 @@ class MathUtilsTest {
 	
 
 	@Test
-	void test() {
+	void testAdd() {
 		
 		MathUtils mathUtils = new MathUtils();
+//		int expected = 1;
 		int expected = 2;
+
 		int actual = mathUtils.add(1, 1);
-		assertEquals(expected, actual);
+		assertEquals(expected, actual, "The add method should add two numbers");
+		
+	}
+	
+	@Test
+	void testComputeCircleRadius() {
+		MathUtils mathUtils = new MathUtils();
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return circle area");
 		
 	}
 
